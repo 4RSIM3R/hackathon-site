e5722647bbb9bf1112fea88c140adc3172ed19ce
M Tsaqif Mukhayyar <m3g3nz2@gmail.com>
2020-11-29 10:37:09 +0700

    pembenahan kata-kata

--- components/home/TesFooter.js
+++ components/home/TesFooter.js
@@ -1,43 +1,49 @@
-import s from '../../styles/Tes.module.css'
-import SiteMaps from '../utils/SiteMaps'
+import s from "../../styles/Tes.module.css";
+import SiteMaps from "../utils/SiteMaps";
 
 const TesFooter = () => {
-    return (
-        <div className={s.footer_section}>
-            <div className={s.footer_content}>
-                <div className={s.maps_wrapper}>
-                    <h3 className={s.maps_title}>Hackathon Locations</h3>
-                    <div className={s.maps_content}>
-                        <SiteMaps/>
-                    </div>
-                </div>
-            </div>
-            <div className={s.footer_content}>
-                <div className={s.content_wrapper}>
-                    <h3 className={s.maps_title}>Nunggu Apa Lagi ?</h3>
-                    <p>Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi standar contoh teks sejak tahun 1500an</p>
-                    <div className={s.content_menu_wrapper}>
-                        <div className={s.content_menu_list}>
-                            <a href="">Facebook</a>
-                            <a href="">Twitter</a>
-                            <a href="">Instagram</a>
-                        </div>
-                        <div className={s.content_menu_list}>
-                            <a href="">Home</a>
-                            <a href="">Get Started</a>
-                            <a href="">FAQ</a>
-                        </div>
-                        <div className={s.content_menu_list}>
-                            <a href="">Sponsorship</a>
-                            <a href="">Timeline</a>
-                            <a href="">Contact Us</a>
-                        </div>
-                    </div>
-                    <p className={s.make_with}>Make With 💙 By Sanjos DEV</p>
-                </div>
-            </div>
+  return (
+    <div className={s.footer_section}>
+      <div className={s.footer_content}>
+        <div className={s.maps_wrapper}>
+          <h3 className={s.maps_title}>Hackathon Locations</h3>
+          <div className={s.maps_content}>
+            <SiteMaps />
+          </div>
         </div>
-    )
-}
+      </div>
+      <div className={s.footer_content}>
+        <div className={s.content_wrapper}>
+          <h3 className={s.maps_title}>Nunggu Apa Lagi ?</h3>
+          <p>
+            Hackathon Santri 2021 adalah kompetisi hackathon yang pertama kali
+            akan ada di pondok pesantren, indonesia, bahkan dunia. Lewat sini
+            kami ingin mendekatkan lagi kata santri dan teknologi yang
+            seringkali bertolak belakang. Dan menekankan peran santri pada
+            perkembangan teknologi.
+          </p>
+          <div className={s.content_menu_wrapper}>
+            <div className={s.content_menu_list}>
+              <a href="">Facebook</a>
+              <a href="">Twitter</a>
+              <a href="">Instagram</a>
+            </div>
+            <div className={s.content_menu_list}>
+              <a href="">Home</a>
+              <a href="">Get Started</a>
+              <a href="">FAQ</a>
+            </div>
+            <div className={s.content_menu_list}>
+              <a href="">Sponsorship</a>
+              <a href="">Timeline</a>
+              <a href="">Contact Us</a>
+            </div>
+          </div>
+          <p className={s.make_with}>Make With 💙 By Sanjos DEV</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default TesFooter
+export default TesFooter;
